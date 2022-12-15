2c94620421a872496fb65b5a064dfb896eca2c36
L41TH <laith.elmershati@gmail.com>
2022-12-15 18:57:14 +0100

    :rocket: deployment script for localhost tests

---
+++ scripts/local_deployment_test.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const Contract = await hre.ethers.getContractFactory("NFTContract");
+  const contract = await Contract.deploy();
+  await contract.deployed();
+  console.log("Contract deployed to:", contract.address);
+
+  const NONBuyableContract = await hre.ethers.getContractFactory("NFTContractNONBuyable");
+  const nonBuyableContract = await NONBuyableContract.deploy();
+  await nonBuyableContract.deployed();
+  console.log("NON Buyable Contract deployed to:", nonBuyableContract.address);
+
+  await contract.mint();
+  await contract.mint();
+  await contract.mint();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
