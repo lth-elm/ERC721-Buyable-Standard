348c23817df3bc7a2afd1780bc3b86f92e557b68
L41TH <laith.elmershati@gmail.com>
2022-12-20 11:30:16 +0100

    :lipstick: rainbowkit accentcolor

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,22 @@ import "./styles/Pages.css";
 import Interface from "./Interface";
 import NotConnected from "./components/NotConnected";
 
+import { useState } from "react";
+
 import "@rainbow-me/rainbowkit/styles.css";
 import {
   getDefaultWallets,
   RainbowKitProvider,
   darkTheme,
+  ConnectButton,
 } from "@rainbow-me/rainbowkit";
-import {
-  chain,
-  useAccount,
-  configureChains,
-  createClient,
-  WagmiConfig,
-} from "wagmi";
+// import { CustomAvatar } from "./components/CustomAvatar";
+
+import { chain, useAccount, configureChains, createClient, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-// import { CustomConnectButton } from "./components/CustomConnectButton";
-import { useState } from "react";
 
 const { chains, provider } = configureChains(
-  [chain.hardhat, chain.goerli, chain.localhost], // [chain.mainnet, chain.goerli, chain.sepolia, chain.polygon, chain.polygonMumbai, chain.optimism, chain.optimismGoerli, chain.arbitrum, chain.arbitrumGoerli, chain.localhost, chain.hardhat, chain.foundry]
+  [chain.hardhat, chain.goerli], // [chain.mainnet, chain.goerli, chain.sepolia, chain.polygon, chain.polygonMumbai, chain.optimism, chain.optimismGoerli, chain.arbitrum, chain.arbitrumGoerli, chain.localhost, chain.hardhat, chain.foundry]
   [publicProvider()]
 );
 const { connectors } = getDefaultWallets({
@@ -43,20 +39,19 @@ function App() {
   const [style, setStyle] = useState("App-header orange");
   const [animation, setAnimation] = useState("toOrange");
   const changeStyle = () => {
-    style === "App-header orange"
-      ? setStyle("App-header light")
-      : setStyle("App-header orange");
+    style === "App-header orange" ? setStyle("App-header light") : setStyle("App-header orange");
     setChecked(!checked);
-    animation === "toLight"
-      ? setAnimation("toOrange")
-      : setAnimation("toLight");
+    animation === "toLight" ? setAnimation("toOrange") : setAnimation("toLight");
   };
 
   const [checked, setChecked] = useState(false);
 
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider theme={darkTheme()} chains={chains}>
+      <RainbowKitProvider
+        theme={darkTheme({ accentColor: "#f8cc82", accentColorForeground: "black" })}
+        chains={chains}
+      >
         <div className="App preload">
           <header className={style}>
             <div className="title">
@@ -64,16 +59,11 @@ function App() {
             </div>
             <div className="header-right">
               <label className="switch">
-                <input
-                  type="checkbox"
-                  checked={checked}
-                  onChange={changeStyle}
-                />
+                <input type="checkbox" checked={checked} onChange={changeStyle} />
                 <span className="slider round"></span>
               </label>
 
               <ConnectButton />
-              {/* <CustomConnectButton /> */}
             </div>
           </header>
 
