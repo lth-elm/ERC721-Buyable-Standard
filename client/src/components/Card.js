3126aded62a8ac5728e72dfe81914f5d69ad45d3
Lorenzo Renouvin <lrenouvin@aubay.com>
2022-12-20 10:47:24 +0100

    :art: new UI

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,7 +2,16 @@ import React from "react";
 import "../styles/Card.css";
 
 export default function Card(props) {
-  const { index, img, name, price, available, buyToken } = props;
+  const {
+    index,
+    img,
+    name,
+    price,
+    available,
+    buyToken,
+    setPrice,
+    removeTokenSale,
+  } = props;
 
   return (
     <div className="nftCard">
@@ -12,41 +21,69 @@ export default function Card(props) {
         </div>
         <div className="name">{name}</div>
       </div>
-      {/* <button
-        className={available ? "buy" : "not_for_sale"}
-        onClick={() => buyToken(index)}
-      >
-        {available ? `${price} ETH | Buy` : "Not for sale"}
-      </button> */}
-      {available ? (
-        <div class="parent">
-          <div class="div1">
-            <input placeholder={`${price} ETH | Buy`} />
-          </div>
-          <div class="div2">
-            <button
-              className={available ? "buy" : "not_for_sale"}
-              onClick={() => buyToken(index)}
-            >
-              Update price
-            </button>
-          </div>
-          <div class="div3">
-            <button
-              className={available ? "buy" : "not_for_sale"}
-              onClick={() => buyToken(index)}
-            >
-              Remove from sale
-            </button>
-          </div>
-        </div>
-      ) : (
+      {buyToken ? (
         <button
-          className={available ? "buy" : ""}
+          className={available ? "buy" : "not_for_sale"}
           onClick={() => buyToken(index)}
         >
-          Add to sale
+          {available ? `${price} ETH | Buy` : "Not for sale"}
         </button>
+      ) : (
+        <>
+          {available ? (
+            <div class="footer-inSale">
+              <div className="child1">
+                <input
+                  placeholder={`${price} ETH`}
+                  type="number"
+                  id={"setNewPrice" + index}
+                />
+
+                <button
+                  className={available ? "buy" : "not_for_sale"}
+                  onClick={() =>
+                    setPrice(
+                      index,
+                      document.getElementById("setNewPrice" + index).value
+                    )
+                  }
+                >
+                  Update price
+                </button>
+              </div>
+
+              <div class="child2">
+                <button
+                  className={available ? "buy" : "not_for_sale"}
+                  onClick={() => removeTokenSale(index)}
+                >
+                  Remove from sale
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="footer-notInSale">
+              <input
+                placeholder={`Price in ETH`}
+                type="number"
+                id={"setNewPrice" + index}
+                step="0.01"
+                min="0"
+              />
+              <button
+                className={available ? "buy" : "sale"}
+                onClick={() =>
+                  setPrice(
+                    index,
+                    document.getElementById("setNewPrice" + index).value
+                  )
+                }
+              >
+                Add to sale
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
