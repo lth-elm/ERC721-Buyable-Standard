db1c22c331fd176498bcbbdbc7ac7f1127bb384b
L41TH <laith.elmershati@gmail.com>
2023-02-14 11:38:26 +0100

    :construction_worker: CI

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
+// const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
 
-const GOERLI_API_URL = process.env.GOERLI_API_URL;
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+// const GOERLI_API_URL = process.env.GOERLI_API_URL;
+// const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -17,16 +17,16 @@ module.exports = {
       },
     },
   },
-  networks: {
-    hardhat: {},
-    goerli: {
-      url: GOERLI_API_URL,
-      accounts: [GOERLI_PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_KEY,
-  },
+  // networks: {
+  //   hardhat: {},
+  //   goerli: {
+  //     url: GOERLI_API_URL,
+  //     accounts: [GOERLI_PRIVATE_KEY],
+  //   },
+  // },
+  // etherscan: {
+  //   apiKey: ETHERSCAN_KEY,
+  // },
   // paths: {
   //   artifacts: './client/src/artifacts'
   // },
